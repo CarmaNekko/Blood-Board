eb922e811638e25309aa5aa74d99ad1a723b17a3
ArthurParadox <gabrieldelzo@hotmail.com>
2026-06-05 02:02:52 -0500

    Material Champion Peon

--- Assets/Scripts/Tutorial/TutorialMaganer.cs
+++ Assets/Scripts/Tutorial/TutorialMaganer.cs
@@ -42,8 +42,8 @@ public class TutorialManager : MonoBehaviour
         UpdateText(textShift, "Presiona \"Shift\" para correr. (0/1)", Color.yellow);
         UpdateText(textSpace, "Presiona \"Space\" para saltar. (0/1)", Color.yellow);
 
-        UpdateText(textLeftClick, "Presionar \"Click Izquierdo\" para disparar magia blanca. (0/1)", Color.yellow);
-        UpdateText(textRightClick, "Presionar \"Click Derecho\" para disparar magia negra. (0/1)", Color.yellow);
+        UpdateText(textLeftClick, "Presionar \"Click Izquierdo\" para disparar magia blanca al guardia negro. (0/1)", Color.yellow);
+        UpdateText(textRightClick, "Presionar \"Click Derecho\" para disparar magia negra al guardia blanco. (0/1)", Color.yellow);
 
         if (combatPanel != null)
         {
@@ -102,13 +102,13 @@ public class TutorialManager : MonoBehaviour
             if (!completedLeftClick && Input.GetMouseButtonDown(0))
             {
                 completedLeftClick = true;
-                UpdateText(textLeftClick, "Presionar \"Click Izquierdo\" para disparar magia blanca. (1/1)", Color.green);
+                UpdateText(textLeftClick, "Presionar \"Click Izquierdo\" para disparar magia blanca al guardia negro. (1/1)", Color.green);
             }
 
             if (!completedRightClick && Input.GetMouseButtonDown(1))
             {
                 completedRightClick = true;
-                UpdateText(textRightClick, "Presionar \"Click Derecho\" para disparar magia negra. (1/1)", Color.green);
+                UpdateText(textRightClick, "Presionar \"Click Derecho\" para disparar magia negra al guardia blanco. (1/1)", Color.green);
             }
 
             if (completedLeftClick && completedRightClick)
